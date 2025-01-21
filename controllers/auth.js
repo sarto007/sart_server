56464daa04db87156721451955ea5d6acc040e2b
minospeed007 <nwannemino@gmail.com>
2025-01-21 11:18:27 +0000

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,13 @@
-import WaitListSchema from '../models/WaitList.js'
+import WaitList from '../models/WaitList.js'
+import User from '../models/Users.js';
+import bcrypt from 'bcryptjs';
 
-export const register= async (req,res,next)=>{
+
+// Add Email to Waitlist
+export const waitList= async (req,res,next)=>{
  console.log(req.body.email);
     try{
-  const newUser= new WaitListSchema({
+  const newUser= new WaitList({
    email: req.body.email
     
 
@@ -29,4 +33,47 @@ export const register= async (req,res,next)=>{
     });
   }
     }
+}
+
+// Create User
+export const createUser= async (req,res,next)=>{
+  try{
+    const salt=bcrypt.genSaltSync(10)
+    const hash=bcrypt.hashSync(req.body.password,salt)
+const newUser= new User({
+  ...req.body,
+  password:hash,})
+  
+await newUser.save()
+res.status(200).send(newUser)
+console.log("user created!")
+  }catch (err) {
+    if (err.code === 11000) { 
+      const field = Object.keys(err.keyPattern)[0]; 
+      res.status(400).send(`The ${field} already exists!`);
+    } else {
+      res.status(500).send('Internal server error');
+    }
+    console.log(err);
+  }
+}
+
+//Login
+
+export const register= async(req,res,next)=>{
+  try{
+const user= await User.findOne({username:req.body.username})
+if(!user) return res.send(404, "user not found");
+const isPasswordCorrect= await bcrypt.compare(req.body.password, user.password)
+if(!isPasswordCorrect) return res.send(400, "wrong password or username");
+const token= jwt.sign({id:user._id, isAdmin: user.isAdmin}, process.env.JWT)
+
+const {password, isAdmin, ...otherDetails}=user._doc
+res.cookie("access_token",token, {
+  httpOnly:true
+}).status(200).json({details:{...otherDetails},isAdmin})
+
+  }catch(err){
+    next(err)
+  }
 }
